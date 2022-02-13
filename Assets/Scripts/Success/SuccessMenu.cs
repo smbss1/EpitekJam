1dfab80d735246633f9641bf68f6a56ce038592c
IKEA-luddes <luddes@ikea.se>
2022-02-13 04:02:35 +0100

    Update SuccessMenu.cs

--- Assets/Scripts/Success/SuccessMenu.cs
+++ Assets/Scripts/Success/SuccessMenu.cs
@@ -7,13 +7,15 @@ public class SuccessMenu : MonoBehaviour
     public GameObject PrefabsSuccessContent;
     public Transform Parent;
 
+    private List<int> idList = new List<int>();
+
     private void OnEnable()
     {
         foreach (int id in StockageManager.instance.SuccessList)
         {
-            if (id < 0 || SuccessStocker.instance.successes.Length <= id)
+            if (id < 0 || SuccessStocker.instance.successes.Length <= id || idList.Contains(id))
                 continue;
-            Debug.Log(SuccessStocker.instance.successes.Length + " " + id);
+            idList.Add(id);
             GameObject SuccessContent = Instantiate(PrefabsSuccessContent, Parent);
             SuccessObject information = SuccessStocker.instance.successes[id];
             SuccessContent.GetComponent<SuccessContent>().MakeDetailSuccess(information.title, information.description);
